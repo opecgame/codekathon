4472d409ba3dca0c526f3cbf4df48908092e22a5
OpecZ CH <me@opecgame.in.th>
2022-06-19 01:06:40 +0700

    Fix DB
    Co-Authored-By: khachenchai <86509369+khachenchai@users.noreply.github.com>

--- Website/modules/mongodb.js
+++ Website/modules/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const config = require("../config");
 module.exports = () => {
-    mongoose.connect("mongodb+srv://pkw:pkw%40)8Em!p%23s2r~6KS.%2F@cluster0.rjnsq.mongodb.net/pkw_2022").then(() => {
+    mongoose.connect("").then(() => {
         console.log(`Mongo Connect`);
     })
 }
